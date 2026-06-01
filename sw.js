0e168b9400ae7b7341ec358b0b4cc78e1ffaead8
TrishabOnlineService <nitai.grp150@gmail.com>
2026-06-01 18:06:35 +0000

    Deploying to gh-pages from @ TrishabOnlineService/Duty-tracker-pro-professional@b223de9d18aefabf0f76c4411abcfeaf0bdd0165 🚀

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-const CACHE_NAME = 'duty-tracker-pro-v3';
+const CACHE_NAME = 'duty-tracker-pro-v4';
 const ASSETS = [
   './',
   './index.html',
-  './src/main.tsx',
-  './src/App.tsx',
-  './src/index.css',
   'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://i.ibb.co/1fngZNzk/icon-512.png'
